704f0f14cfd60f3b2a6d51cc8570c0f7962ee427
hassan1910 <1910ibrahimhassan@gmail.com>
2024-12-20 17:32:48 +0300

    solved code-challenge

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,16 +5,16 @@ from models.magazine import Magazine
 
 class TestModels(unittest.TestCase):
     def test_author_creation(self):
-        author = Author(1, "John Doe")
-        self.assertEqual(author.name, "John Doe")
+        author = Author(1, "Hassan Ibrahim")
+        self.assertEqual(author.name, "Hassan Ibrahim")
 
     def test_article_creation(self):
         article = Article(1, "Test Title", "Test Content", 1, 1)
         self.assertEqual(article.title, "Test Title")
 
     def test_magazine_creation(self):
-        magazine = Magazine(1, "Tech Weekly")
-        self.assertEqual(magazine.name, "Tech Weekly")
+        magazine = Magazine(1, "PULSE")
+        self.assertEqual(magazine.name, "PULSE")
 
 if __name__ == "__main__":
     unittest.main()
